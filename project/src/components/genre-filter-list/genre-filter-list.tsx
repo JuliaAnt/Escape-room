320fe0621b3962848b30ba7f35e61c661bafb6a6
Julia Antoshkina <julia.antoshkina@gmail.com>
2023-05-14 22:06:59 +0200

    Added readme

--- project/src/components/genre-filter-list/genre-filter-list.tsx
+++ project/src/components/genre-filter-list/genre-filter-list.tsx
@@ -1,10 +1,11 @@
 import { GENRE_FILTERS } from '../../consts';
+import { GenreFilter } from '../../types/filters';
 import GenreFilterItem from '../genre-filter-item/genre-filter-item';
 
 function GenreFilterList(): JSX.Element {
   return (
     <ul className="filter__list">
-      {GENRE_FILTERS.map((filter) => <GenreFilterItem key={filter.type} filter={filter} />)}
+      {GENRE_FILTERS.map((filter: GenreFilter) => <GenreFilterItem key={filter.type} filter={filter} />)}
     </ul>
   );
 
--- project/src/components/level-filter-list/level-filter-list.tsx
+++ project/src/components/level-filter-list/level-filter-list.tsx
@@ -1,10 +1,11 @@
 import { LEVEL_FILTERS } from '../../consts';
+import { LevelFilter } from '../../types/filters';
 import LevelFilterItem from '../level-filter-item/level-filter-item';
 
 function LevelFilterList(): JSX.Element {
   return (
     <ul className="filter__list">
-      {LEVEL_FILTERS.map((filter) => <LevelFilterItem key={filter.type} filter={filter} />)}
+      {LEVEL_FILTERS.map((filter: LevelFilter) => <LevelFilterItem key={filter.type} filter={filter} />)}
     </ul>
   );
 
